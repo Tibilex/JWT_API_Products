5bd06a3d1bd0ae0ef223e972fb439e1f4aea3f5c
Tibilex <45212175+Tibilex@users.noreply.github.com>
2022-11-19 10:50:10 +0200

    Add: Product Model

---
+++ JWT_API_Products/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace JWT_API_Products.Models
+{
+    public class Product
+    { 
+        public int ProductId { get; set; }
+        public string? ProductName { get; set; }
+        public string? ProductDescription { get; set; }
+        public int ProductCost { get; set; }
+        public int ProductStock { get; set; }
+        
+    }
+}
