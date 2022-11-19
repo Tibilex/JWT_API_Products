61004749e55f4c564c1b52ffc8d79873fd2c2032
Tibilex <45212175+Tibilex@users.noreply.github.com>
2022-11-19 11:06:05 +0200

    Add: Product Controller

---
+++ JWT_API_Products/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JWT_API_Products.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
