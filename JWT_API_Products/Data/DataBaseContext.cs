292d701c577b8ec965f57f75000ca05ab8fb3788
Tibilex <45212175+Tibilex@users.noreply.github.com>
2022-11-19 10:52:48 +0200

    Add: DB Context

---
+++ JWT_API_Products/Data/DataBaseContext.cs
@@ -0,0 +1,19 @@
+﻿using JWT_API_Products.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JWT_API_Products.Data
+{
+    public class DataBaseContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+        public DataBaseContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
